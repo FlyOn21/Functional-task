cc44f25857f7bfabfd63ed10a52f93f9315ba094
Zhoholiev <pzhogolev@perfsys.com>
2024-03-13 10:27:24 +0200

    Refactor Printer.java used string format 13.03.24 10:27 Zhoholiev

--- src/app/utils/Printer.java
+++ src/app/utils/Printer.java
@@ -14,28 +14,28 @@ public class Printer {
     }
     private static void printAllData(List<User> users) {
         StringBuilder sb = new StringBuilder();
-        users.forEach(user -> sb.append("===================")
-                .append(" User Name: ").append(user.getUsername()).append(" ===================").append("\n")
-                .append("Email: ").append(user.getEmail()).append("\n")
-                .append("Phone: ").append(user.getPhone()).append("\n")
-                .append("Name: ").append(user.getName()).append("\n")
-                .append("Salt: ").append(user.getSalt()).append("\n")
-                .append("IsActive: ").append(user.isActive()? "Yes" : "No").append("\n")
-                .append("IsAdmin: ").append(user.isAdmin() ? "Yes" : "No").append("\n")
-                .append("Creation Timestamp: ").append(user.getCreateTimestamp()).append("\n")
-                .append("Last Update Timestamp: ").append(user.getUpdateTimestamp())
-                .append("\n")
-                .append("\n"));
+        users.forEach(user -> sb.append(String.format(
+                "=================== User Name: %s ===================\n" +
+                        "Email: %s\n" +
+                        "Phone: %s\n" +
+                        "Name: %s\n" +
+                        "Salt: %s\n" +
+                        "IsActive: %s\n" +
+                        "IsAdmin: %s\n" +
+                        "Creation Timestamp: %s\n" +
+                        "Last Update Timestamp: %s\n\n",
+                user.getUsername(), user.getEmail(), user.getPhone(), user.getName(),
+                user.getSalt(), user.isActive() ? "Yes" : "No", user.isAdmin() ? "Yes" : "No",
+                user.getCreateTimestamp(), user.getUpdateTimestamp())));
         System.out.println(sb);
     }
 
     private static void printSharedData(List<User> users) {
         StringBuilder sb = new StringBuilder();
-        users.forEach(user -> sb.append("===================")
-                .append(" User Name: ").append(user.getUsername()).append(" ===================").append("\n")
-                .append("Email: ").append(user.getEmail()).append("\n")
-                .append("\n")
-                .append("\n"));
+        users.forEach(user -> sb.append(String.format(
+                "=================== User Name: %s ===================\n" +
+                        "Email: %s\n\n",
+                user.getUsername(), user.getEmail())));
         System.out.println(sb);
     }
 }
